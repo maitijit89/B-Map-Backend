edaa450e0fa96f0f779b7bc897c21664acec5cad
maitijit89 <maitidebjit2@gmail.com>
2026-05-17 03:02:58 +0530

    fix: add missing os import in router.go to resolve build failure

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/maitijit89/B-Map-Backend
 
-go 1.22.0
+go 1.25.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.4
--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"os"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
